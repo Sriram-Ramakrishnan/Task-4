1e1d4364f0c8ca34f3117663e46d0a340f1fe496
uday.arora <uday1o1arora@gmail.com>
2022-09-17 15:01:07 +0530

    updates common/dev/prod config files

--- webpack.common.js
+++ webpack.common.js
@@ -1,37 +1,30 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+
 module.exports = {
-    entry: {
-       bundle: path.resolve(__dirname, 'src/index.js')
-    }, 
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader','css-loader','sass-loader']
-            },
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                  loader: 'babel-loader',
-                  options: {
-                    presets: ['@babel/preset-env']
-                  }
-                }
-              }
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-        title: 'Webpack Application',
-        filename: 'index.html',
-        template: 'src/template.html'
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Login',
-            filename: 'Login.html',
-            template: 'src/Login.html'
-            })
-    ]
-}
+  entry: {
+    main: "./src/index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"],
+          },
+        },
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,24 +1,42 @@
-const path = require('path');
+const path = require("path");
 const common = require("./webpack.common");
-const { merge } = require("webpack-merge");
+const merge = require("webpack-merge").merge;
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = merge(common,{
-    mode: 'development',
-    entry: {
-        bundle: path.resolve(__dirname, 'src/index.js')
-     },
-    output: {
-        path: path.resolve(__dirname,'dist'),
-        filename: '[name].js',
-    },
-    devServer : {
-        static: {
-            directory: path.resolve(__dirname, 'dist'),
-        },
-        port: 3000,
-        open: true,
-        hot: true,
-        compress: true,
-        historyApiFallback: true,    
-    },
+module.exports = merge(common, {
+  mode: "development",
+  devtool: false,
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[name][ext]",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Application",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Login",
+      filename: "Login.html",
+      template: "src/Login.html",
+    }),
+  ],
 });
